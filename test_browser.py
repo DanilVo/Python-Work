fc8d838473db77d047263e63a0a83c0a0b33265d
DanilV <Dani736@gmail.com>
2022-03-10 22:18:11 +0200

    added dice

--- test_browser.py
+++ test_browser.py
@@ -1,3 +1,8 @@
 import webbrowser
 
-webbrowser.open('http://net-informations.com/python/net/browser.htm#:~:text=Using%20the%20web%20browser%20in,and%20use%20open()%20function.')
+
+j = int(input())
+if j == 1:
+    webbrowser.open('http://net-informations.com/python/net/browser.htm#:~:text=Using%20the%20web%20browser%20in,and%20use%20open()%20function.')
+else:
+    print('no')
